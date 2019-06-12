297cc2e819681ff904cc4bf7d7ec844e5e2ea105
kuter <contact@devktr.pl>
2019-06-13 00:34:39 +0200

    Bump version: 0.0.0 → 0.0.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='version-control',
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(exclude=["*.swp"]),
     install_requires=[
         'Django >=2.1,<2.2',
--- version_control/__init__.py
+++ version_control/__init__.py
@@ -53,4 +53,4 @@ Templates
 
 .. program-output:: tree ../../version_control/templates/
 """
-__version__ = '0.0.0'
+__version__ = '0.0.1'
